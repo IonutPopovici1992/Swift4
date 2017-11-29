f77bd184ee3b4ef8a6e572b54a13574a8939f28c
John <ionut_popovici92@yahoo.com>
2017-11-29 17:49:48 +0200

    Added Enumerations.playground

---
+++ Enumerations.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+enum NameOfEnum {
+    case caseOne, caseTwo, caseThree
+}
+
+let enumeration: NameOfEnum = .caseOne
+
+
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+productBarcode = .qrCode("abcdefghijklmnopqrstuvwxyz")
+
+switch productBarcode {
+    case let .upc(numberSystem, manufacturer, productCode, check):
+        print("UPC: \(numberSystem), \(manufacturer), \(productCode), \(check)")
+    case let .qrCode(productCode):
+        print("QR CODE: \(productCode)")
+}
+
+
+enum JediMaster: String {
+    case yoda = "Yoda"
+    case maceWindu = "Mace Windu"
+    case quiGonJin = "Qui-Gon Jin"
+    case obiWanKenobi = "Obi-Wan Kenobi"
+    case lukeSkywalker = "Luke Skywalker"
+}
+
+print(JediMaster.yoda.rawValue)
+
+
+enum SwitchStatus {
+    case on
+    case off
+}
+
+var switchStatus: SwitchStatus = .off
+
+func flipSwitch(status: SwitchStatus) -> SwitchStatus {
+    if status == .off {
+        return .on
+    } else {
+        return .off
+    }
+}
+
+flipSwitch(status: switchStatus)
+
+switchStatus = .on
+
+flipSwitch(status: switchStatus)
