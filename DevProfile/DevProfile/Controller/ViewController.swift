a9b1089513f8503fa2a321e320fcfbfbd26f7be2
John <ionut_popovici92@yahoo.com>
2018-04-24 11:39:53 +0300

    Added DevProfile app

---
+++ DevProfile/DevProfile/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DevProfile
+//
+//  Created by John on 4/3/18.
+//  Copyright © 2018 John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
